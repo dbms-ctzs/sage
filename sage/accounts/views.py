acb904f20b6495c070e20943875f5cb10012eeb4
DevSPP <subratprasad.mail@gmail.com>
2020-04-24 23:48:17 +0530

    accounts/profile dummy view added without query to data base and without ID

--- sage/accounts/views.py
+++ sage/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse # To send raw HTTP Response-Remove is if not required ~subrat
 
 # Create your views here.
+
+# Temp data for testing: REMOVE ~subrat
+data = {
+        "fname": "subrat",
+        "lname": "prasad",
+        "email": "subrat@sage.com",
+        "rank": 1,
+        "badge":"Great Sage",
+    }
+
+# Profile View for the users profile
+# Profile/id will be actual URL once the database is functional ~subrat
+def profile(request):
+    return render(request, "accounts/profile.html",{
+        "data": data,
+    })
